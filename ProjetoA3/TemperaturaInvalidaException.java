1c2b3aecf1d4efceccc737c6d22c47473c0f5c53
RachelCoelho <163034541+RachelCoelho@users.noreply.github.com>
2025-06-23 08:46:28 -0300

    Update TemperaturaInvalidaException.java

--- ProjetoA3/TemperaturaInvalidaException.java
+++ ProjetoA3/TemperaturaInvalidaException.java
@@ -1,7 +1,12 @@
 package temperatura;
 
+// Declara uma nova exceção personalizada chamada TemperaturaInvalidaException
 public class TemperaturaInvalidaException extends Exception {
+
+    // Construtor da exceção que recebe uma mensagem de erro como parâmetro
     public TemperaturaInvalidaException(String mensagem) {
+        // Chama o construtor da classe Exception para armazenar a mensagem
         super(mensagem);
     }
 }
+
